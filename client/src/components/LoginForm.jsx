9cfce9b9ef23b6d5df19c8944c5eab1b24a544c2
Tomas Vargas <tavargas9@gmail.com>
2024-02-14 13:01:03 -0700

    add log-in functionality

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,6 +9,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, {error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -26,14 +27,16 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await loginUser(userFormData);
+      const { data } = await login({
+        variables: userFormData
+      });
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
+      console.log(data)
+      const token = data.login.user;
 
-      const { token, user } = await response.json();
-      console.log(user);
       Auth.login(token);
     } catch (err) {
       console.error(err);
